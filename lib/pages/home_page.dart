581d3e40f232e4f3763a18df6776a69b7b1b9624
realYungi <realyungijeongprimary@gmail.com>
2024-02-21 14:04:11 +0900

    Added basic UI design for the login, signup screen, and created a posting feature with the design of the post box.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:uridachi/components/my_textfield.dart';
+import 'package:uridachi/components/wallpost.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -9,13 +11,19 @@ class HomePage extends StatelessWidget {
   final currentUser = FirebaseAuth.instance.currentUser!;
   final textController = TextEditingController();
 
-  void signUserOut () {
+  void signUserOut() {
     FirebaseAuth.instance.signOut();
-
   }
 
-
-  void postMessage () {
+  void postMessage() {
+    //making sure only to post contents where the textfields are filled in 
+    if (textController.text.isNotEmpty) {
+      FirebaseFirestore.instance.collection("User Posts").add({
+        'UserEmail' : currentUser.email,
+        'Message' : textController.text,
+        'TimeStamp' : Timestamp.now(),
+      });
+    }
 
   }
 
@@ -25,39 +33,72 @@ class HomePage extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("日韓 SNS"),
-        actions: [IconButton(onPressed: signUserOut, 
-        icon: Icon(Icons.logout))],
+        actions: [IconButton(onPressed: signUserOut, icon: Icon(Icons.logout))],
         backgroundColor: Colors.white,
       ),
-      body: Column(
-        children: [
-
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: MyTextField(
-                    controller: textController,
-                    hintText: 'Write something on the wall',
-                    obscureText: false,
-                    height: 30,
-                  ),
-                  ),
-            
-            
-                  IconButton(
-                    onPressed: postMessage, 
-                    icon: const Icon(Icons.arrow_circle_up
+      body: Center(
+        child: Column(
+          children: [
+            Expanded(
+              child: StreamBuilder( 
+                //ordering by the time the posts have been posted
+                stream: FirebaseFirestore.instance
+                    .collection("User Posts")
+                    .orderBy(
+                      "TimeStamp",
+                      descending: false,
                     )
-                  ),
-            
-              ],
+                    .snapshots(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return ListView.builder(
+                          itemCount: snapshot.data!.docs.length,
+                    
+                          itemBuilder: (context, index){
+        
+                          final post = snapshot.data!.docs[index];
+                          return WallPost(
+                            message: post['Message'], 
+                            user: post['UserEmail'], 
+              
+                            
+                          );
+        
+                        }, 
+                        );
+                      } else if (snapshot.hasError) {
+                        return Center(
+                          child: Text('Error:${snapshot.error}'),
+                        );
+        
+                      }
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    },
+              ),
             ),
-          ),
-
-          Text("Logged in as : " + currentUser.email!),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: MyTextField(
+                      controller: textController,
+                      hintText: 'Write something on the wall',
+                      obscureText: false,
+                      height: 30,
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: postMessage,
+                      icon: const Icon(Icons.arrow_circle_up)),
+                ],
+              ),
+            ),
+            Text("Logged in as : " + currentUser.email!),
+          ],
+        ),
       ),
     );
   }
